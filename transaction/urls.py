41475916d8f95c80f45cd31d15313b7f89109882
Sheik <mohamed.sheik.ali1127@zohomail.in>
2022-01-27 12:05:52 +0530

    url-added

--- transaction/urls.py
+++ transaction/urls.py
@@ -11,19 +11,25 @@ from .views import (
     TransitionDeleteApiview
 )
 from graphene_django.views import GraphQLView
-# from .schema import schema
+from .schema import schema
 
 urlpatterns = [
     path('program/', ProgramCreateApiView.as_view(), name='pros'),
-    path('program/<int:pk>/', ProgramUpdateDeleteApiview.as_view(),name='program-update'),
+    path('program/<int:pk>/', ProgramUpdateDeleteApiview.as_view(),
+         name='program-update'),
     path('program-list/', ProgramListApiView.as_view(), name='pro-list'),
-    path('program/transition/delete/<int:pk>/', TransitionDeleteApiview.as_view(),name='delete-transition'),
-    path('program/transition/submit/<int:pk>/',SubmitTransitionApiView.as_view(),name='initial-submit'),
-    path('program/transition/reject/<int:pk>/',RejectedSignApi.as_view(),name='reject-transition'),
-    path('program/transition/submit/', include('transaction.url.submit'),name='program-transition-approvals-SUBMIT'),
-    path('program/transition/reject/', include('transaction.url.reject'),name='program-transition-reject-REJECT'),
-    path('action/',ActionCreateApiView.as_view()),
-    path('model/',ModelCreateApiview.as_view()),
+    path('program/transition/delete/<int:pk>/',
+         TransitionDeleteApiview.as_view(), name='delete-transition'),
+    path('program/transition/submit/<int:pk>/',
+         SubmitTransitionApiView.as_view(), name='initial-submit'),
+    path('program/transition/reject/<int:pk>/',
+         RejectedSignApi.as_view(), name='reject-transition'),
+    path('program/transition/submit/', include('transaction.url.submit'),
+         name='program-transition-approvals-SUBMIT'),
+    path('program/transition/reject/', include('transaction.url.reject'),
+         name='program-transition-reject-REJECT'),
+    path('action/', ActionCreateApiView.as_view()),
+    path('model/', ModelCreateApiview.as_view()),
     path('messages/', include('transaction.url.message')),
-
+    path('graphql/', GraphQLView.as_view(graphiql=True, schema=schema))
 ]
