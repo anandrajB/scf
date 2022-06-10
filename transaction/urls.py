9821b1235c78ca7d3658248ea1c08925ebe79f1f
anandrajB <anand98.ar@gmail.com>
2022-06-10 12:26:28 +0530

    updated views

--- transaction/urls.py
+++ transaction/urls.py
@@ -83,7 +83,7 @@ urlpatterns = [
     path('workflowitem/<int:pk>/',WorkFlowItemUpdateApi.as_view(),name='workflowitem-update-api'),
     path('workevents/<int:pk>/',WorkEventsUpdateApi.as_view() , name = 'workevent_update_api'),
     path('notification/',InboxNotificationCountApiView.as_view(),name='notification-api-unread-messages-count'),
-    path('choices/',MiscApiView.as_view(),name = 'interest and rate_type list api ')
+    path('choices/',MiscApiView.as_view(),name = 'interest and rate_type list api '),
 
     #### TRANSITIONS ####
 
